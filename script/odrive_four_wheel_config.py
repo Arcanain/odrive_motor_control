91d2cba83d58a533ceebf5257c4a74d510736b02
Ramune6110 <arduino6110@gmail.com>
2022-10-09 15:05:32 +0900

    [add]four wheel vertion

---
+++ script/odrive_four_wheel_config.py
@@ -0,0 +1,231 @@
+#! /usr/bin/env python3
+import sys
+import time
+import odrive
+from odrive.enums import *
+import fibre.libfibre
+
+class OdriveConfiguration:
+    def __init__(self):
+        # Connect to Odrive
+        self.find_odrive()
+    
+    def find_odrive(self):
+        while True:
+            print("Connect to Odrive...")
+            #self.odrv0 = odrive.find_any()
+            self.odrv0 = odrive.find_any(serial_number="208434855748")
+            self.odrv1 = odrive.find_any(serial_number="207B346D5748")
+
+            if self.odrv0 is not None and self.odrv1 is not None:
+                print("Connect to Odrive Success!!!")
+                break
+            else:
+                print("Disconnect to Odrive...")
+                
+    def set_odrive_parameters(self):
+        # Connect to Odrive
+        self.find_odrive()
+        
+        # odrv0 parameter config
+        self.odrv0.axis0.motor.config.pole_pairs = 15
+        self.odrv0.axis0.motor.config.resistance_calib_max_voltage = 4
+        self.odrv0.axis0.motor.config.requested_current_range = 25 
+        self.odrv0.axis0.motor.config.current_control_bandwidth = 100
+
+        self.odrv0.axis1.motor.config.pole_pairs = 15
+        self.odrv0.axis1.motor.config.resistance_calib_max_voltage = 4
+        self.odrv0.axis1.motor.config.requested_current_range = 25 
+        self.odrv0.axis1.motor.config.current_control_bandwidth = 100
+
+        self.odrv0.axis0.motor.config.torque_constant = 8.27 / 16
+        self.odrv0.axis0.encoder.config.mode = ENCODER_MODE_HALL
+        self.odrv0.axis0.encoder.config.cpr = 90
+        self.odrv0.axis0.encoder.config.calib_scan_distance = 150
+
+        self.odrv0.axis1.motor.config.torque_constant = 8.27 / 16
+        self.odrv0.axis1.encoder.config.mode = ENCODER_MODE_HALL
+        self.odrv0.axis1.encoder.config.cpr = 90
+        self.odrv0.axis1.encoder.config.calib_scan_distance = 150
+
+        self.odrv0.axis0.encoder.config.bandwidth = 100
+        self.odrv0.axis0.controller.config.pos_gain = 1
+        self.odrv0.axis0.controller.config.vel_gain = 0.02 * self.odrv0.axis0.motor.config.torque_constant * self.odrv0.axis0.encoder.config.cpr
+        self.odrv0.axis0.controller.config.vel_integrator_gain = 0.1 * self.odrv0.axis0.motor.config.torque_constant * self.odrv0.axis0.encoder.config.cpr
+        self.odrv0.axis0.controller.config.vel_limit = 10
+
+        self.odrv0.axis1.encoder.config.bandwidth = 100
+        self.odrv0.axis1.controller.config.pos_gain = 1
+        self.odrv0.axis1.controller.config.vel_gain = 0.02 * self.odrv0.axis0.motor.config.torque_constant * self.odrv0.axis1.encoder.config.cpr
+        self.odrv0.axis1.controller.config.vel_integrator_gain = 0.1 * self.odrv0.axis0.motor.config.torque_constant * self.odrv0.axis1.encoder.config.cpr
+        self.odrv0.axis1.controller.config.vel_limit = 10
+
+        self.odrv0.axis0.controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
+        self.odrv0.axis1.controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
+
+        self.odrv0.config.enable_brake_resistor = True
+        
+        self.odrv0.axis0.motor.config.current_lim = 5
+        self.odrv0.axis0.motor.config.calibration_current = 5
+        self.odrv0.axis0.encoder.config.hall_polarity_calibrated = True 
+
+        self.odrv0.axis1.motor.config.current_lim = 5
+        self.odrv0.axis1.motor.config.calibration_current = 5
+        self.odrv0.axis1.encoder.config.hall_polarity_calibrated = True 
+
+        # odrv1 parameter config
+        self.odrv1.axis0.motor.config.pole_pairs = 15
+        self.odrv1.axis0.motor.config.resistance_calib_max_voltage = 4
+        self.odrv1.axis0.motor.config.requested_current_range = 25 
+        self.odrv1.axis0.motor.config.current_control_bandwidth = 100
+
+        self.odrv1.axis1.motor.config.pole_pairs = 15
+        self.odrv1.axis1.motor.config.resistance_calib_max_voltage = 4
+        self.odrv1.axis1.motor.config.requested_current_range = 25 
+        self.odrv1.axis1.motor.config.current_control_bandwidth = 100
+
+        self.odrv1.axis0.motor.config.torque_constant = 8.27 / 16
+        self.odrv1.axis0.encoder.config.mode = ENCODER_MODE_HALL
+        self.odrv1.axis0.encoder.config.cpr = 90
+        self.odrv1.axis0.encoder.config.calib_scan_distance = 150
+
+        self.odrv1.axis1.motor.config.torque_constant = 8.27 / 16
+        self.odrv1.axis1.encoder.config.mode = ENCODER_MODE_HALL
+        self.odrv1.axis1.encoder.config.cpr = 90
+        self.odrv1.axis1.encoder.config.calib_scan_distance = 150
+
+        self.odrv1.axis0.encoder.config.bandwidth = 100
+        self.odrv1.axis0.controller.config.pos_gain = 1
+        self.odrv1.axis0.controller.config.vel_gain = 0.02 * self.odrv1.axis0.motor.config.torque_constant * self.odrv1.axis0.encoder.config.cpr
+        self.odrv1.axis0.controller.config.vel_integrator_gain = 0.1 * self.odrv1.axis0.motor.config.torque_constant * self.odrv1.axis0.encoder.config.cpr
+        self.odrv1.axis0.controller.config.vel_limit = 10
+
+        self.odrv1.axis1.encoder.config.bandwidth = 100
+        self.odrv1.axis1.controller.config.pos_gain = 1
+        self.odrv1.axis1.controller.config.vel_gain = 0.02 * self.odrv1.axis0.motor.config.torque_constant * self.odrv1.axis1.encoder.config.cpr
+        self.odrv1.axis1.controller.config.vel_integrator_gain = 0.1 * self.odrv1.axis0.motor.config.torque_constant * self.odrv1.axis1.encoder.config.cpr
+        self.odrv1.axis1.controller.config.vel_limit = 10
+
+        self.odrv1.axis0.controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
+        self.odrv1.axis1.controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
+
+        self.odrv1.config.enable_brake_resistor = True
+        
+        self.odrv1.axis0.motor.config.current_lim = 5
+        self.odrv1.axis0.motor.config.calibration_current = 5
+        self.odrv1.axis0.encoder.config.hall_polarity_calibrated = True 
+
+        self.odrv1.axis1.motor.config.current_lim = 5
+        self.odrv1.axis1.motor.config.calibration_current = 5
+        self.odrv1.axis1.encoder.config.hall_polarity_calibrated = True 
+        
+        # https://discourse.odriverobotics.com/t/problem-with-save-configuration-using-python/7581
+        print("Saving manual configuration and rebooting...")
+        try:
+            print("Save Configuration")
+            self.odrv0.save_configuration()
+            self.odrv1.save_configuration()
+        except fibre.libfibre.ObjectLostError:
+            pass # Saving configuration makes the device reboot
+        
+        try:
+            print("Odrive reboot")
+            self.odrv0.reboot()
+            self.odrv1.reboot()
+        except:
+            pass
+        
+    def motor_calibration(self):
+        # Connect to Odrive
+        self.find_odrive()
+    
+        self.odrv0.axis0.requested_state = AXIS_STATE_MOTOR_CALIBRATION
+        self.odrv1.axis0.requested_state = AXIS_STATE_MOTOR_CALIBRATION
+        # Wait for calibration to take place
+        time.sleep(10)
+        self.odrv0.axis0.motor.config.pre_calibrated = True
+        self.odrv1.axis0.motor.config.pre_calibrated = True
+
+        self.odrv0.axis1.requested_state = AXIS_STATE_MOTOR_CALIBRATION
+        self.odrv1.axis1.requested_state = AXIS_STATE_MOTOR_CALIBRATION
+        # Wait for calibration to take place
+        time.sleep(10)
+        self.odrv0.axis1.motor.config.pre_calibrated = True
+        self.odrv1.axis1.motor.config.pre_calibrated = True
+    
+    def encoder_calibration(self):
+        self.odrv0.axis0.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION
+        self.odrv1.axis0.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION
+        # Wait for calibration to take place
+        time.sleep(30)
+        self.odrv0.axis0.encoder.config.pre_calibrated = True
+        self.odrv1.axis0.encoder.config.pre_calibrated = True
+        
+        self.odrv0.axis1.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION
+        self.odrv1.axis1.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION
+        # Wait for calibration to take place
+        time.sleep(30)
+        self.odrv0.axis1.encoder.config.pre_calibrated = True
+        self.odrv1.axis1.encoder.config.pre_calibrated = True
+        
+        # https://discourse.odriverobotics.com/t/problem-with-save-configuration-using-python/7581
+        print("Saving manual configuration and rebooting...")
+        try:
+            print("Save Configuration")
+            self.odrv0.save_configuration()
+            self.odrv1.save_configuration()
+        except fibre.libfibre.ObjectLostError:
+            pass # Saving configuration makes the device reboot
+        
+        try:
+            print("Odrive reboot")
+            self.odrv0.reboot()
+            self.odrv1.reboot()
+        except:
+            pass
+    
+    def config(self):
+        print("Start set_odrive_parameters")
+        self.set_odrive_parameters()
+        print("Finish set_odrive_parameters")
+        
+        print("Start motor_calibration")
+        self.motor_calibration()
+        print("Finish motor_calibration")
+        
+        print("Start encoder_calibration")
+        self.encoder_calibration()
+        print("Finish encoder_calibration")
+
+    def operation_check(self):
+        # Connect to Odrive
+        self.find_odrive()
+        
+        print("Motor operation check...")
+        
+        # odrv0 control
+        self.odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+        self.odrv0.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+        self.odrv0.axis0.controller.input_vel = 2
+        self.odrv0.axis1.controller.input_vel = -2
+
+        # odrv1 control
+        self.odrv1.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+        self.odrv1.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+        self.odrv1.axis0.controller.input_vel = 2
+        self.odrv1.axis1.controller.input_vel = -2
+
+        # Wait for calibration to take place
+        time.sleep(10)
+        self.odrv0.axis0.controller.input_vel = 0
+        self.odrv0.axis1.controller.input_vel = 0
+
+        self.odrv1.axis0.controller.input_vel = 0
+        self.odrv1.axis1.controller.input_vel = 0
+        
+        print("Motor operation check Success!!!")
+        
+if __name__ == "__main__":
+    Odrive_motor_config = OdriveConfiguration()
+    Odrive_motor_config.config()
+    #Odrive_motor_config.operation_check()
---
+++ script/odrive_twist_driver_four_wheel.py
@@ -0,0 +1,292 @@
+#! /usr/bin/env python3
+import rospy
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import TransformStamped
+from nav_msgs.msg import Odometry
+from nav_msgs.msg import Path
+from geometry_msgs.msg import PoseStamped
+
+import tf.transformations
+import tf_conversions
+import tf2_ros
+
+import sys
+import time
+import odrive
+from odrive.enums import *
+import fibre.libfibre
+import math
+
+class OdriveMotorControl:
+    def __init__(self):
+        # Connect to Odrive
+        self.find_odrive()
+        
+        # setup parameter
+        self.tire_tread         = 0.32                                         #[m] distance between wheel centres
+        self.target_linear_vel  = 0.0                                          #[m/s]
+        self.target_angular_vel = 0.0                                          #[rad/s]
+        self.tire_diameter      = 0.165                                        #[m]
+        self.right_wheel_radius = self.tire_diameter                           #[m]
+        self.left_wheel_radius  = self.tire_diameter                           #[m]
+        self.encoder_cpr        = 90.0                                         #[count]
+        self.tire_circumference = math.pi * self.tire_diameter                 #[m]
+        self.m_t_to_value       = 1.0 / (self.tire_circumference)              #[turns/s]
+        self.m_s_to_value       = self.encoder_cpr / (self.tire_circumference) #[count/s]
+        self.vel_l = 0.0     #[count/s]
+        self.vel_r = 0.0     #[count/s]
+        self.new_pos_l = 0.0 #[count]
+        self.new_pos_r = 0.0 #[count]
+        self.old_pos_l = 0.0 #[count]
+        self.old_pos_r = 0.0 #[count]
+
+        # store current location to be updated. 
+        self.x = 0.0     #[m]
+        self.y = 0.0     #[m]
+        self.theta = 0.0 #[rad]
+
+        # subscriber cmd_vel
+        rospy.Subscriber("cmd_vel", Twist, self.callback_vel)
+
+        # publish odom
+        self.odom_publisher = rospy.Publisher("odom", Odometry, tcp_nodelay=True, queue_size=10)
+
+        # publish odom_path
+        self.odom_path_publisher = rospy.Publisher("odom_path", Path, tcp_nodelay=True, queue_size=10)
+        self.poses_list = []
+
+        # setup message
+        # https://qiita.com/honeytrap15/items/550c757f2964b575883c
+        #self.odom_frame = rospy.get_param('~odom_frame', "odom")
+        #self.base_frame = rospy.get_param('~base_frame', "base_link")
+
+        self.odom_frame = "map"
+        self.base_frame = "base_link"
+
+        self.odom_msg = Odometry()
+        self.odom_msg.header.frame_id = self.odom_frame
+        self.odom_msg.child_frame_id  = self.base_frame
+        self.odom_msg.pose.pose.position.x = 0.0
+        self.odom_msg.pose.pose.position.y = 0.0
+        self.odom_msg.pose.pose.position.z = 0.0    # always on the ground, we hope
+        self.odom_msg.pose.pose.orientation.x = 0.0 # always vertical
+        self.odom_msg.pose.pose.orientation.y = 0.0 # always vertical
+        self.odom_msg.pose.pose.orientation.z = 0.0
+        self.odom_msg.pose.pose.orientation.w = 1.0
+        self.odom_msg.twist.twist.linear.x = 0.0
+        self.odom_msg.twist.twist.linear.y = 0.0  # no sideways
+        self.odom_msg.twist.twist.linear.z = 0.0  # or upwards... only forward
+        self.odom_msg.twist.twist.angular.x = 0.0 # or roll
+        self.odom_msg.twist.twist.angular.y = 0.0 # or pitch... only yaw
+        self.odom_msg.twist.twist.angular.z = 0.0
+
+        # setup transform
+        self.map_broadcaster  = tf.TransformBroadcaster()
+        self.odom_broadcaster = tf.TransformBroadcaster()
+
+        self.map_to_odom_msg = TransformStamped()
+        self.map_to_odom_msg.header.frame_id = "map"
+        self.map_to_odom_msg.child_frame_id  = "odom"
+        self.map_to_odom_msg.transform.translation.x = 0.0
+        self.map_to_odom_msg.transform.translation.y = 0.0
+        self.map_to_odom_msg.transform.translation.z = 0.0
+        self.map_to_odom_msg.transform.rotation.x = 0.0
+        self.map_to_odom_msg.transform.rotation.y = 0.0
+        self.map_to_odom_msg.transform.rotation.w = 0.0
+        self.map_to_odom_msg.transform.rotation.z = 1.0
+
+        self.odom_to_baselink_msg = TransformStamped()
+        self.odom_to_baselink_msg.header.frame_id = "map"
+        self.odom_to_baselink_msg.child_frame_id  = "base_link"
+        self.odom_to_baselink_msg.transform.translation.x = 0.0
+        self.odom_to_baselink_msg.transform.translation.y = 0.0
+        self.odom_to_baselink_msg.transform.translation.z = 0.0
+        self.odom_to_baselink_msg.transform.rotation.x = 0.0
+        self.odom_to_baselink_msg.transform.rotation.y = 0.0
+        self.odom_to_baselink_msg.transform.rotation.w = 0.0
+        self.odom_to_baselink_msg.transform.rotation.z = 1.0
+
+    def find_odrive(self):
+        while True:
+            print("Connect to Odrive...")
+            self.odrv0 = odrive.find_any(serial_number="208434855748")
+            self.odrv1 = odrive.find_any(serial_number="207B346D5748")
+
+            if self.odrv0 is not None and self.odrv1 is not None:
+                print("Connect to Odrive Success!!!")
+                break
+            else:
+                print("Disconnect to Odrive...")
+    
+    def odrive_setup(self):
+        # Connect to Odrive
+        #self.find_odrive()
+
+        # ordv0 
+        self.odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+        self.odrv0.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+        self.odrv0.axis0.controller.input_vel = 0
+        self.odrv0.axis1.controller.input_vel = 0
+        # ordv1
+        self.odrv1.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+        self.odrv1.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+        self.odrv1.axis0.controller.input_vel = 0
+        self.odrv1.axis1.controller.input_vel = 0
+    
+    def calcodom(self, current_time):
+        # https://docs.odriverobotics.com/v/latest/commands.html
+        ######################
+        # Calcurate Position #
+        ######################
+        # convert [turn] into [count]
+        self.new_pos_r = self.encoder_cpr * self.odrv0.axis0.encoder.pos_estimate #[count]
+        self.new_pos_l = self.encoder_cpr * self.odrv0.axis1.encoder.pos_estimate #[count]
+
+        delta_pos_r = self.new_pos_r - self.old_pos_r #[count]
+        delta_pos_l = self.new_pos_l - self.old_pos_l #[count]
+        
+        self.old_pos_r = self.new_pos_r #[count]
+        self.old_pos_l = self.new_pos_l #[count]
+        
+        # Check for overflow. Assume we can't move more than half a circumference in a single timestep. 
+        half_cpr = self.encoder_cpr / 2.0
+        if delta_pos_r >  half_cpr: 
+            delta_pos_r = delta_pos_r - self.encoder_cpr
+        elif delta_pos_r < -half_cpr: 
+            delta_pos_r = delta_pos_r + self.encoder_cpr
+        if delta_pos_l >  half_cpr: 
+            delta_pos_l = delta_pos_l - self.encoder_cpr
+        elif delta_pos_l < -half_cpr: 
+            delta_pos_l = delta_pos_l + self.encoder_cpr
+        
+        # convert [turns] into [m]
+        delta_pos_r_m = delta_pos_r / self.m_s_to_value
+        delta_pos_l_m = delta_pos_l / self.m_s_to_value * (-1)
+        
+        # Distance travelled
+        d = (delta_pos_r_m + delta_pos_l_m) / 2.0  # delta_ps
+        th = (delta_pos_r_m - delta_pos_l_m) / self.tire_tread # works for small angles
+    
+        xd = math.cos(th)*d
+        yd = -math.sin(th)*d
+
+        # Pose: updated from previous pose + position delta
+        self.x += math.cos(self.theta)*xd - math.sin(self.theta)*yd
+        self.y += math.sin(self.theta)*xd + math.cos(self.theta)*yd
+        self.theta = (self.theta + th) % (2*math.pi)
+
+        # Twist/velocity: calculated from motor values only
+        self.vel_r = self.encoder_cpr * self.odrv0.axis0.encoder.vel_estimate
+        self.vel_l = self.encoder_cpr * self.odrv0.axis1.encoder.vel_estimate * (-1)
+        v = self.tire_circumference * (self.vel_r + self.vel_l) / (2.0*self.encoder_cpr)
+        w = self.tire_circumference * (self.vel_r - self.vel_l) / (self.tire_tread * self.encoder_cpr) # angle: vel_r*tyre_radius - vel_l*tyre_radius
+        #print(v)
+        #sprint(w)
+        
+        ####################
+        # Publish Odometry #
+        ####################
+        self.odom_msg.header.stamp = current_time
+        self.odom_msg.pose.pose.position.x = self.x
+        self.odom_msg.pose.pose.position.y = self.y
+        q = tf_conversions.transformations.quaternion_from_euler(0.0, 0.0, self.theta)
+        self.odom_msg.pose.pose.orientation.z = q[2] # math.sin(self.theta)/2
+        self.odom_msg.pose.pose.orientation.w = q[3] # math.cos(self.theta)/2
+        self.odom_msg.twist.twist.linear.x  = v
+        self.odom_msg.twist.twist.angular.z = w
+        self.odom_publisher.publish(self.odom_msg)
+
+        ######################################
+        # transform odom_frame to base_frame #
+        ######################################
+        odom_quat = tf.transformations.quaternion_from_euler(0.0, 0.0, 0.0)
+        self.map_broadcaster.sendTransform(
+            (0.0, 0.0, 0.0),
+            odom_quat,
+            rospy.Time.now(),
+            "odom",
+            "map"
+        )
+
+        self.odom_broadcaster.sendTransform(
+            (self.x, self.y, 0.0),
+            q,
+            rospy.Time.now(),
+            "base_link",
+            "odom"
+        )
+
+        #########################
+        # Publish Odometry Path #
+        #########################
+        temp_pose = PoseStamped()
+        temp_pose.header.stamp = rospy.Time.now()
+        temp_pose.header.frame_id = "map"
+        temp_pose.pose.position.x = self.x
+        temp_pose.pose.position.y = self.y
+        temp_pose.pose.orientation.z = q[2]
+        temp_pose.pose.orientation.w = q[3]
+
+        self.poses_list.append(temp_pose)
+
+        # creat path data
+        self.path = Path()
+        self.path.header.stamp = rospy.Time.now()
+        self.path.header.frame_id = "map"
+        self.path.poses = self.poses_list
+
+        self.odom_path_publisher.publish(self.path)
+
+    def odrive_control(self):
+        rate = rospy.Rate(50)
+        while not rospy.is_shutdown():
+            # current time
+            current_time = rospy.Time.now()
+        
+            # calculate relative velocity
+            right_vel, left_vel = self.calc_relative_vel(self.target_linear_vel, self.target_angular_vel)
+            
+            try:
+                # Get current position
+                self.calcodom(current_time)
+
+                # Set velocity
+                self.odrv0.axis0.controller.input_vel = right_vel
+                self.odrv0.axis1.controller.input_vel = -left_vel
+                self.odrv1.axis0.controller.input_vel = right_vel
+                self.odrv1.axis1.controller.input_vel = -left_vel
+                
+                # Time sleep
+                rate.sleep()
+            except AttributeError as error:
+                # Output expected AttributeErrors.
+                rospy.signal_shutdown(error)
+            except KeyboardInterrupt:
+                self.odrv0.axis0.controller.input_vel = 0
+                self.odrv0.axis1.controller.input_vel = 0
+                self.odrv1.axis0.controller.input_vel = 0
+                self.odrv1.axis1.controller.input_vel = 0
+
+                rospy.signal_shutdown("KeyboardInterrupt")
+                
+    def calc_relative_vel(self, target_linear_vel, target_angular_vel):
+        # Convert to each circumferential velocity
+        circumferential_right_vel = target_linear_vel + (self.tire_tread / 2.0) * target_angular_vel #[m/s]
+        circumferential_left_vel  = target_linear_vel - (self.tire_tread / 2.0) * target_angular_vel #[m/s]
+
+        # Convert to each rotational velocity
+        right_vel = circumferential_right_vel / self.tire_circumference #[turn/s]
+        left_vel  = circumferential_left_vel / self.tire_circumference  #[turn/s]
+
+        return right_vel, left_vel
+    
+    def callback_vel(self, msg):
+        self.target_linear_vel = msg.linear.x
+        self.target_angular_vel = msg.angular.z
+    
+if __name__ == "__main__":
+    rospy.init_node("odrive_motor_control", disable_signals=True)
+    Odrive_motor_control = OdriveMotorControl()
+    Odrive_motor_control.odrive_setup()
+    Odrive_motor_control.odrive_control()
+    rospy.spin()
